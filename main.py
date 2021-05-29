eebb6c96747235f960d8b1c3fad2034c2dfa199f
pavelzag <zagalsky@gmail.com>
2021-05-29 21:55:48 +0300

    some changes for the object

--- main.py
+++ main.py
@@ -14,27 +14,38 @@ PUT = "PUT"
 GITHUB_BASE_URL = "https://api.github.com"
 OWNER = "pytest-dev"
 REPO = "pytest"
+PR_STATES = ['open']
+# PR_STATES = ['open', 'closed']
 
 
 @app.route("/get_repos_list", methods=['GET'])
 def get_repos_list():
     content = request.args
-    pr_state = content['state']
     g = Github(GITHUB_TOKEN)
     repo = g.get_repo(f'{OWNER}/{REPO}')
-    pull_requests = repo.get_pulls(state=pr_state)
-    print(f'There are {pull_requests.totalCount} {pr_state} pull requests')
-    all_test_runs = []
-    for pr in pull_requests:
-        print(pr)
-        pr_head_commit_has = pr.head.sha
-        check_runs_url = f"{GITHUB_BASE_URL}/repos/{OWNER}/{REPO}/commits/{pr_head_commit_has}/check-runs"
-        headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-        r = requests.request(url=check_runs_url, method=GET, headers=headers)
-        check_runs = loads(r.content)['check_runs']
-        all_test_runs.append(check_runs)
-    print(all_test_runs)
-    return Response(json.dumps(all_test_runs), status=200, mimetype='application/json')
+    all_prs_object = {}
+    for pr_state in PR_STATES:
+        pull_requests = repo.get_pulls(state=pr_state)
+        print(f'There are {pull_requests.totalCount} {pr_state} pull requests')
+        for pr in pull_requests:
+            all_prs_object[pr.title] = {}
+            pr_head_commit_has = pr.head.sha
+            check_runs_url = f"{GITHUB_BASE_URL}/repos/{OWNER}/{REPO}/commits/{pr_head_commit_has}/check-runs"
+            headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+            r = requests.request(url=check_runs_url, method=GET, headers=headers)
+            check_runs = loads(r.content)['check_runs']
+            changed_files_url = f"{GITHUB_BASE_URL}/repos/{OWNER}/{REPO}/pulls/{pr.raw_data['number']}/files"
+            r = requests.request(url=changed_files_url, method=GET, headers=headers)
+            changed_files = loads(r.content)
+            for changed_file in changed_files:
+                changed_file_filename = [changed_file['filename']]
+                if 'changed_filename' in all_prs_object[pr.title]:
+                    all_prs_object[pr.title]['changed_filenames'].append(changed_file_filename[0])
+                else:
+                    all_prs_object[pr.title]['changed_filenames'] = changed_file_filename
+            all_prs_object[pr.title]['check_runs'] = check_runs
+    print(all_prs_object)
+    return Response(json.dumps(all_prs_object), status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
